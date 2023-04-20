afba8fefeb50d59ddf01650f533590a874e42a79
DINH Tran Thai Duc <ducthai2001177@gmail.com>
2023-04-20 12:10:52 +0200

    Update Test

--- src/Test/TestReadAllMoves.java
+++ src/Test/TestReadAllMoves.java
@@ -1,16 +1,9 @@
 package Test;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Reader;
-import java.util.HashMap;
-import java.util.Scanner;
 
 import org.apache.commons.csv.*;
 
@@ -20,7 +13,7 @@ public class TestReadAllMoves {
         // Read a file
         String filePath = "./testData/testDivide.csv";
         
-        String[] HEADERS =  {"move_pkid","move_id","year","month","departure","arrival","from_id","to_id","from_name","to_name","from_type","to_type","vessel_id","vessel_name","vessel_type","dwt","time"};
+        //String[] HEADERS =  {"move_pkid","move_id","year","month","departure","arrival","from_id","to_id","from_name","to_name","from_type","to_type","vessel_id","vessel_name","vessel_type","dwt","time"};
         // Parese
         Reader in = new FileReader(filePath);
         CSVFormat csvFormat = CSVFormat.DEFAULT .builder()
@@ -59,5 +52,6 @@ public class TestReadAllMoves {
             }
             printer.printRecord(record);
         }
+        csvParser.close();
     }
 }
