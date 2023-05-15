e83ecb8529bd49aa63669207fe693fb4f1657708
DINH Tran Thai Duc <ducthai2001177@gmail.com>
2023-05-15 16:43:01 +0200

    Minor change

--- src/Test/TestCheckCoherent2.java
+++ src/Test/TestCheckCoherent2.java
@@ -7,8 +7,13 @@ public class TestCheckCoherent2
     public static void main(String[] args) {
         Move m1,m2,m3,m4;
         System.out.println("Ecart > 10");
+        
         m1 = new Move ("01/03/1977 00:00","01/03/1977 00:00",1,2);
         m2 = new Move ("14/03/1977 00:00","15/03/1977 00:00",3,4);
+        System.out.println (m1);
+        System.out.println (m2);
+        System.out.println("------------------------");
+
         for (Move m :Move.checkCoherent2(m1,m2))
         {
             System.out.println(m);
@@ -16,6 +21,9 @@ public class TestCheckCoherent2
         System.out.println("Ecart <= 10 and Ecart >5");
         m3 = new Move ("01/03/1977 00:00","01/03/1977 00:00",1,2);
         m4 = new Move ("8/03/1977 00:00","15/03/1977 00:00",3,4);
+        System.out.println (m3);
+        System.out.println (m4);
+        System.out.println("------------------------");
         for (Move m :Move.checkCoherent2(m3,m4))
         {
             System.out.println(m);
@@ -23,6 +31,10 @@ public class TestCheckCoherent2
         System.out.println("Ecart <= 5");
         m3 = new Move ("01/03/1977 00:00","01/03/1977 00:00",1,2);
         m4 = new Move ("07/03/1977 00:00","15/03/1977 00:00",3,4);
+        System.out.println (m3);
+        System.out.println (m4);
+        System.out.println("------------------------");
+
         for (Move m :Move.checkCoherent2(m3,m4))
         {
             System.out.println(m);
