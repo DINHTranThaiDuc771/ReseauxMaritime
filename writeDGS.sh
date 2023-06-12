e72adecb6d74e74e6e6380e55826b2f2f49b3441
DINH Tran Thai Duc <ducthai2001177@gmail.com>
2023-06-12 10:08:54 +0200

    Delete unecessary

--- writeDGS.sh
+++
@@ -1,7 +0,0 @@
-javac -encoding utf8 "@compile.list"
-./getDate.sh "./testData/testDGS.csv"
-if [ -f "graphDynamic.dgs" ]; then
-    echo "OverWrite..."
-    rm -f graphDynamic.dgs
-fi
-java -cp ./bin Executable.WriteDGSFile
